8ddedf27ec86f80d45d4801957e9ec9282ca2369
Oskar Rough <oskar@rough.dk>
2022-03-26 23:10:13 +0100

    Refactor actions

--- public/actions.js
+++ public/actions.js
@@ -32,121 +32,20 @@ export function newGame() {
 	}
 }
 
-// This action runs on every frame.
-export function tick(baseState, delta) {
-	let nextState = baseState
-
-	// Clear any expired spells.
-	if (baseState.castTime === 0 && baseState.castingSpellId) {
-		nextState = finishCast(baseState, delta)
-	}
-
-	return produce(nextState, (draft) => {
-		const now = performance.now()
-		const oldPlayer = nextState.player
-		const oldTank = nextState.party.tank
-
-		// Update internal time tracking.
-		draft.config.elapsedTime = now - nextState.beginningOfTime
-		draft.ticks = nextState.ticks + 1
-
-		// Apply any status effects on the tank.
-		oldTank.effects.forEach((effect, index) => {
-			if (effect.ticks > 0) {
-				const timeSince = performance.now() - effect.appliedAt
-				const msPerTick = effect.duration / effect.appliedTicks
-				if (timeSince > msPerTick) {
-					const healPerTick = effect.heal / effect.appliedTicks
-					draft.party.tank.health = clamp(
-						oldTank.health + healPerTick,
-						0,
-						oldTank.baseHealth
-					)
-					draft.party.tank.effects[index].ticks--
-					draft.party.tank.effects[index].appliedAt = performance.now()
-				}
-			} else {
-				draft.party.tank.effects.splice(index, 1)
-			}
-		})
-
-		if (nextState.config.elapsedTime > 1) {
-			// Slowly reduce the tank's healt.
-			// draft.party.tank.health = draft.party.tank.health - 0.25 * (draft.ticks / 80)
-			draft.party.tank.health--
-		}
-
-		// Regenerate mana after X seconds
-		if (oldPlayer.lastCastTime) {
-			const timeSince = now - oldPlayer.lastCastTime
-			if (timeSince > 2000) {
-				draft.player.mana = clamp(oldPlayer.mana + 0.7, 0, oldPlayer.baseMana)
-			}
-		}
-
-		// Count down cast time, if needed
-		if (nextState.castTime > 0) {
-			const newTime = nextState.castTime - delta
-			draft.castTime = newTime > 0 ? newTime : 0
-		}
-
-		// Reset global cooldown.
-		if (nextState.gcd > 0) {
-			const newTime = nextState.gcd - delta
-			draft.gcd = newTime > 0 ? newTime : 0
-		}
-
-		// Stop game if the tank has died.
-		if (oldTank.health < 0) {
-			draft.party.tank.health = 0
-			draft.gameOver = true
-		}
-	})
-}
-
-export function castSpell(baseState, spellId) {
-	const spell = spells[spellId]
-	// const sameSpell = spellId === baseState.castingSpellId
-
-	if (baseState.gcd > 0) {
-		throw new Error('can not cast while there is global cooldown')
-	}
-
-	if (spell.cost > baseState.player.mana) {
-		throw new Error('not enough mana')
-	}
-
-	if (window.webhealer.castTimer) {
-		log('clearing existing spell cast', {old: baseState.castingSpellId, new: spellId})
-		clearTimeout(window.webhealer.castTimer)
-	}
-
-	return produce(baseState, (draft) => {
-		log('started casting', spell.name)
-		draft.castingSpellId = spellId
-		draft.castTime = spell.cast
-		draft.gcd = baseState.config.globalCooldown
-	})
-}
-
-function finishCast(baseState, delta) {
-	const spell = spells[baseState.castingSpellId]
-
-	return produce(baseState, (draft) => {
-		log('finished casting', spell.name)
-
-		draft.player.mana = baseState.player.mana - spell.cost
-
+function updateEffects(state, delta) {
+	return produce(state, (draft) => {
+		const spell = spells[state.castingSpellId]
 		if (spell.ticks) {
-			console.log('finishing renew', spell.ticks, delta)
-
-			const alreadyHasEffect = baseState.party.tank.effects.findIndex(
+			log('finishing renew', spell.ticks, delta)
+			const alreadyExists = state.party.tank.effects.findIndex(
 				(e) => e.name === spell.name
 			)
-			if (alreadyHasEffect > -1) {
+			if (alreadyExists > -1) {
+				log('reducing ticks')
 				// Refresh existing effect.
-				draft.party.tank.effects[alreadyHasEffect].ticks = spell.ticks
+				draft.party.tank.effects[alreadyExists].ticks = spell.ticks
 			} else {
+				log('applying new renew')
 				// Apply new effect.
 				draft.party.tank.effects.push({
 					...spell,
@@ -155,21 +54,129 @@ function finishCast(baseState, delta) {
 				})
 			}
 		} else {
-			const newHp = baseState.party.tank.health + spell.heal
-			draft.party.tank.health = clamp(newHp, 0, baseState.party.tank.baseHealth)
+		}
+	})
+}
+
+// This action runs on every frame.
+export function tick(state, delta) {
+	// Clear any expired spells.
+	if (state.castTime === 0 && state.castingSpellId) {
+		state = updateEffects(state, delta)
+		state = finishCast(state, delta)
+	}
+
+	return produce(state, (draft) => {
+		const now = performance.now()
+		const player = state.player
+		const tank = state.party.tank
+
+		// Update internal time tracking.
+		draft.config.elapsedTime = now - state.beginningOfTime
+		draft.ticks = state.ticks + 1
+
+		// Count down cast time
+		if (state.castTime > 0) {
+			const newTime = state.castTime - delta
+			draft.castTime = newTime > 0 ? newTime : 0
 		}
 
-		draft.player.lastCastTime = performance.now()
+		// Count down global cooldown
+		if (state.gcd > 0) {
+			const newTime = state.gcd - delta
+			draft.gcd = newTime > 0 ? newTime : 0
+		}
+
+		// Apply any status effects on the tank.
+		tank.effects.forEach((effect, index) => {
+			if (effect.ticks > 0) {
+				const timeSince = now - effect.appliedAt
+				const msPerTick = effect.duration / effect.appliedTicks
+
+				if (timeSince > msPerTick) {
+					const healPerTick = effect.heal / effect.appliedTicks
+					draft.party.tank.health = clamp(tank.health + healPerTick, 0, tank.baseHealth)
+					draft.party.tank.effects[index].ticks--
+					draft.party.tank.effects[index].appliedAt = now
+				}
+			} else {
+				// Remove the effect
+				draft.party.tank.effects.splice(index, 1)
+			}
+		})
+
+		// Slowly reduce the tank's healt (with a short delay)
+		if (state.config.elapsedTime > 1) {
+			// draft.party.tank.health = draft.party.tank.health - 0.25 * (draft.ticks / 80)
+			draft.party.tank.health--
+		}
+
+		// Regenerate mana after X seconds
+		if (player.lastCastTime) {
+			const timeSince = now - player.lastCastTime
+			if (timeSince > 2000) {
+				draft.player.mana = clamp(player.mana + 0.7, 0, player.baseMana)
+			}
+		}
+
+		// Stop game if the tank has died.
+		if (tank.health < 0) {
+			draft.party.tank.health = 0
+			draft.gameOver = true
+		}
+	})
+}
+
+export function castSpell(state, spellId) {
+	const spell = spells[spellId]
+	// const sameSpell = spellId === state.castingSpellId
+
+	if (state.gcd > 0) {
+		throw new Error('can not cast while there is global cooldown')
+	}
+
+	if (spell.cost > state.player.mana) {
+		throw new Error('not enough mana')
+	}
+
+	if (window.webhealer.castTimer) {
+		log('clearing existing spell cast', {old: state.castingSpellId, new: spellId})
+		clearTimeout(window.webhealer.castTimer)
+	}
+
+	return produce(state, (draft) => {
+		log('started casting', spell.name)
+		draft.castingSpellId = spellId
+		draft.castTime = spell.cast
+		draft.gcd = state.config.globalCooldown
+	})
+}
+
+function finishCast(state, delta) {
+	const now = performance.now()
+	const spell = spells[state.castingSpellId]
+
+	return produce(state, (draft) => {
+		log('finished casting', spell.name)
+
+		// Regular spells without ticks
+		if (!spell.ticks) {
+			const newHp = state.party.tank.health + spell.heal
+			draft.party.tank.health = clamp(newHp, 0, state.party.tank.baseHealth)
+		}
+
+		draft.player.mana = state.player.mana - spell.cost
+		draft.player.lastCastTime = now
 		delete draft.castingSpellId
 		delete window.webhealer.castTimer
 	})
 }
 
-export function interrupt(baseState) {
-	console.log('interrupt')
-	return produce(baseState, (draft) => {
+export function interrupt(state) {
+	log('interrupt')
+	return produce(state, (draft) => {
 		clearTimeout(window.webhealer.castTimer)
-		draft.gcd = baseState.config.globalCooldown
+		draft.gcd = 0
 		draft.castTime = 0
 		draft.castingSpellId = null
 	})
