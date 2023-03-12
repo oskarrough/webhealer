6e72271f72823b9fd44d7c114b0ad3d6daae4b61
oskarrough <oskar@rough.dk>
2023-03-12 19:17:27 +0100

    Refactor EffectIcon component

--- src/ui.ts
+++ src/ui.ts
@@ -3,6 +3,7 @@ import {html, roundOne} from './utils'
 import {Meter} from './components/bar'
 import Monitor from './components/monitor'
 import SpellIcon from './components/spell-icon'
+import EffectIcon from './components/effect-icon'
 import {WebHealer} from './game-loop'
 import Player from './nodes/player'
 import Tank from './nodes/tank'
@@ -32,76 +33,66 @@ export default function UI(game: WebHealer) {
 	const spell = player.lastCastSpell
 	const timeSinceCast = game.timeSince(player.lastCastTime)
 
-	return html`
-		<div class="Game" onkeyup=${handleShortcuts} tabindex="0">
-			<figure class="Game-bg"></figure>
-			<div class="PartyGroup">
-				<div class="FloatingCombatText"></div>
+	return html`<div class="Game" onkeyup=${handleShortcuts} tabindex="0">
+		<figure class="Game-bg"></figure>
+		<div class="PartyGroup">
+			<div class="FloatingCombatText"></div>
 
-				${game.gameOver
-					? html` <h2>Game Over!</h2>
-							<p>You survived for ${roundOne(game.elapsedTime / 1000)} seconds</p>`
-					: html`<p>
-							<em>"I'm being attacked by an invisible monster! Help! Heal me!"</em>
-					  </p>`}
+			${game.gameOver
+				? html` <h2>Game Over!</h2>
+						<p>You survived for ${roundOne(game.elapsedTime / 1000)} seconds</p>`
+				: html`<p>
+						<em>"I'm being attacked by an invisible monster! Help! Heal me!"</em>
+				  </p>`}
 
-				<br /><br />
+			<br /><br />
 
-				<img src="/assets/ragnaros.webp" width="120" alt="" />
+			<img src="/assets/ragnaros.webp" width="120" alt="" />
 
-				${Meter({
-					type: 'health',
-					value: tank.health,
-					max: tank.baseHealth,
-					potentialValue: spell?.heal,
-					spell: spell,
-				})}
+			${Meter({
+				type: 'health',
+				value: tank.health,
+				max: tank.baseHealth,
+				potentialValue: spell?.heal,
+				spell: spell,
+			})}
 
-				<ul class="Effects">
-					${tank.children.map(
-						(effect) => html`
-							<div class="Spell">
-								<div class="Spell-inner">
-									<h3>${effect.name}</h3>
-									<span> <span class="spin">⏲</span> ${effect.cycles} </span>
-								</div>
-							</div>
-						`
-					)}
-				</ul>
+			<ul class="Effects">
+				${tank.children.map(EffectIcon)}
+			</ul>
+		</div>
+
+		<div class="Player">
+			<div style="min-height: 2.5rem">
+				<p .hidden=${!spell}>
+					Casting ${player.lastCastSpell} ${roundOne(timeSinceCast / 1000)}
+				</p>
+				${spell
+					? Meter({
+							type: 'cast',
+							value: timeSinceCast,
+							max: spell.delay,
+					  })
+					: null}
 			</div>
 
-			<div class="Player">
-				<div style="min-height: 2.5rem">
-					<p .hidden=${!spell}>
-						Casting ${player.lastCastSpell} ${roundOne(timeSinceCast / 1000)}
-					</p>
-					${spell
-						? Meter({
-								type: 'cast',
-								value: timeSinceCast,
-								max: spell.delay,
-						  })
-						: null}
-				</div>
+			<p>Mana</p>
+			${Meter({type: 'mana', value: player.mana, max: player.baseMana})}
+		</div>
 
-				<p>Mana</p>
-				${Meter({type: 'mana', value: player.mana, max: player.baseMana})}
-			</div>
+		<div class="ActionBar">
+			${Object.keys(player.spellbook).map((name, i) => SpellIcon(game, name, i + 1))}
+		</div>
 
-			<div class="ActionBar">
-				${Object.keys(player.spellbook).map((name, i) => SpellIcon(game, name, i + 1))}
-			</div>
+		${Monitor(game)}
+		<div
+			class="Combatlog"
+			onclick=${(event: Event) =>
+				(event.currentTarget as Element).classList.toggle('sticky')}
+		>
+			<ul class="Log Log--scroller"></ul>
+		</div>
 
-			${Monitor(game)}
-			<div
-				class="Combatlog"
-				onclick=${(event: Event) =>
-					(event.currentTarget as Element).classList.toggle('sticky')}
-			>
-				<ul class="Log Log--scroller"></ul>
-			</div>
-
-			<audio loop ?muted=${audio.disabled}></audio>
-		</div>`
+		<audio loop ?muted=${audio.disabled}></audio>
+	</div>`
 }
