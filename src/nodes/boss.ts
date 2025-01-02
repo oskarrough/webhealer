58e1a20b985c91ef61a00433f961e63ecb881903
oskarrough <oskar@rough.dk>
2025-01-02 14:56:41 +0100

    Tweak boss damage

--- src/nodes/boss.ts
+++ src/nodes/boss.ts
@@ -1,47 +1,45 @@
 import {Node, Task} from 'vroum'
 import {randomIntFromInterval} from '../utils'
 import Tank from './tank'
+import {html} from '../utils'
 
-class DamageEffect extends Task {
-	repeat = Infinity
-	duration = 0 // run each cycle for X milliseconds
-	delay = 2000 // wait before running the first cycle of the task
-	interval = 1500 // wait between successive animation cycles
+/**
+ * This is an example boss that has three different attacks.
+ */
+export default class Boss extends Node {
+	build() {
+		const smallAttack = DamageEffect.new({damage: () => randomIntFromInterval(2, 10)})
 
-	damage() {
-		return randomIntFromInterval(2, 10)
-	}
+		const mediumAttack = DamageEffect.new({delay: 3000, interval: 4000})
+		mediumAttack.damage = () => randomIntFromInterval(400, 750)
 
-	tick = () => {
-		// const step = Math.round(loop.deltaTime / 16)
-		const tank = this.Loop.query(Tank)!
-		const x = this.damage()
-		// const isOdd = this.cycles % 2 === 0
-		tank.health = tank.health - x
-		// logger.warn(`Tank took ${x} damage`)
-		const container = document.querySelector('.FloatingCombatText')!
-		const fct = document.createElement('floating-combat-text')
-		fct.textContent = `-${x}`
-		container.appendChild(fct)
+		const hugeAttack = DamageEffect.new({delay: 6000, interval: 8000})
+		hugeAttack.damage = () => randomIntFromInterval(1100, 1500)
+
+		return [smallAttack, mediumAttack, hugeAttack]
 	}
 }
 
-export default class Boss extends Node {
-	build() {
-		const eff1 = DamageEffect.new()
+class DamageEffect extends Task {
+	repeat = Infinity
+	delay = 0 // delay the first cycle
+	duration = 0 // tick once every cycle
+	interval = 1500 // wait between cycles
 
-		const eff2 = DamageEffect.new()
-		eff2.delay = 1000
-		eff2.duration = 2
-		eff2.damage = () => randomIntFromInterval(400, 600)
-		eff2.interval = 1500
+	tick = () => {
+		// Deal damage to our hardcoded tank target
+		const damage = this.damage()
+		const target = this.Loop.query(Tank)!
+		target.health = target.health - damage
 
-		const eff3 = DamageEffect.new()
-		eff3.delay = 2000
-		eff3.duration = 0
-		eff3.damage = () => randomIntFromInterval(900, 1200)
-		eff3.interval = 5000
+		// Create a floating combat text element for the UI
+		const fct = html`<floating-combat-text>-${damage}</floating-combat-text>`.toDOM()
+		const container = document.querySelector('.FloatingCombatText')!
+		container.appendChild(fct)
+	}
 
-		return [eff1, eff2, eff3]
+	/* Overwrite this method to return the damage you need */
+	damage() {
+		return 0
 	}
 }
